6c4c2e90bb38790ac7cfd8225ca6d391abeba3f7
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-01-16 06:05:30 -0500

    used .find() method to search an array, and wrote other functions that do the same thing

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // code your solution here
+
+
+
+function superbowlWin(record){
+    let value = null;
+    let foundItem = record.find(function(item){
+        return item.result == 'W';
+        
+        })
+   if( foundItem){
+    return foundItem.year;
+   }
+   else{
+    return undefined;
+   }
+}
+
+
+/* ----- This also passes --------
+function finder(array, func){
+    let result = null;
+    for(let item of array){
+        if(func(item) === true){
+            result = item;
+            break;
+        }
+    }
+    return result;
+}
+
+console.log(finder(record, superbowlWin));
+
+---------------------------------- */
+
+
+
+
+
+/* This also passes */
+function superbowlWin2(record) {
+    for(let i = 0; i < record.length; i++){
+        if(record[i].result == "W"){
+            return record[i].year;
+        }
+    }
+}
+
